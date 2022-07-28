0f1f21f44d043b4a7bb3fc9718b2a9fbb3c3ea40
PrinceWallyy <olvc.pierre.onghena@gmail.com>
2022-07-28 20:33:28 +0200

    flinkage

--- hpcs/nn/models/_hyp_hc.py
+++ hpcs/nn/models/_hyp_hc.py
@@ -225,8 +225,10 @@ class SimilarityHypHC(pl.LightningModule):
         best_k = 0
         best_pred = None
         for k in range(1, n_clusters + 5):
-            # print(k)
+            print(k)
             y_pred = fcluster(linkage_matrix, k, criterion='maxclust') - 1
+            print(len(y))
+            print(len(y_pred))
             k_ri = ri(y, y_pred)
             if k_ri > best_ri:
                 best_ri = k_ri
