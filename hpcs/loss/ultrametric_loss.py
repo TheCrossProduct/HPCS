533315b5370e9f011fc268469e5434b90db9c9cc
Leonardo Gigli <leonardo.gigli@thecrossproduct.com>
2022-07-31 23:41:21 +0200

    [LOSS] defined special class to compute loss function for SimilarityHypHC

--- hpcs/loss/ultrametric_loss.py
+++ hpcs/loss/ultrametric_loss.py
@@ -1,6 +1,100 @@
 import torch
+from torch.nn import functional as F
 import numpy as np
-# import higra as hg
+from pytorch_metric_learning.losses import BaseMetricLossFunction, TripletMarginLoss
+from pytorch_metric_learning.utils import loss_and_miner_utils as lmu
+from hpcs.distances.poincare import HyperbolicDistance, HyperbolicLCA
+from pytorch_metric_learning.distances import CosineSimilarity, LpDistance
+from typing import Union
+
+class TripletHyperbolicLoss(BaseMetricLossFunction):
+    def __init__(self, sim_distance: str = 'cosine', margin: float = 1.0, init_rescale: float = 1e-3, max_scale: float = 1. - 1e-3,
+                 temperature: float = 0.05, anneal: float = 0.5):
+        super(TripletHyperbolicLoss, self).__init__()
+        # Triplet Loss on similarities between embeddings
+        if sim_distance == 'cosine':
+            self.distace_sim = CosineSimilarity()
+            # self.loss_triplet_sim = losses.TripletMarginLoss(distance=self.distace_sim, margin=self.margin)
+        elif sim_distance == 'hyperbolic':
+            self.distace_sim = HyperbolicDistance()
+            # self.loss_triplet_sim = losses.TripletMarginLoss(distance=self.distace_sim, margin=self.margin)
+        elif sim_distance == 'euclidean':
+            self.distace_sim = LpDistance()
+            # self.loss_triplet_sim = losses.TripletMarginLoss(distance=self.distace_sim, margin=self.margin,
+            #                                                  embedding_regularizer=regularizers.LpRegularizer())
+        else:
+            raise ValueError(
+                f"The option {sim_distance} is not available for sim_distance. The only available are ['cosine', 'euclidean', 'hyperbolic'].")
+
+        self.margin = margin
+
+        self.rescale = torch.nn.Parameter(torch.Tensor([init_rescale]), requires_grad=True)
+        self.max_scale = max_scale
+        self.temperature = temperature
+        self.anneal = anneal
+
+        # distances to compute losses
+        self.loss_triplet_sim = TripletMarginLoss(distance=self.distace_sim, margin=self.margin)
+        # loss between embeddings in
+        self.distance_lca = HyperbolicLCA()
+
+    def anneal(self):
+        # TODO: review this function
+        max_temp = 0.8
+        min_temp = 0.01
+        self.temperature = self.temperature = max(min(self.temperature * self.anneal, max_temp), min_temp)
+    def _rescale_emb(self, embeddings):
+        """Normalize leaves embeddings to have the lie on a diameter."""
+        min_scale = 1e-4  # self.init_size
+        max_scale = self.max_scale
+        return F.normalize(embeddings, p=2, dim=1) * self.rescale.clamp_min(min_scale).clamp_max(max_scale)
+
+    def compute_loss(self, embeddings, labels, indices_tuple, ref_emb, ref_labels, t_per_anchor = 100):
+        indices_tuple = lmu.convert_to_triplets(None, labels, t_per_anchor=t_per_anchor)
+
+        anchor_idx, positive_idx, negative_idx = indices_tuple
+        if len(anchor_idx) == 0:
+            return self.zero_losses()
+
+        if isinstance(self.distace_sim, CosineSimilarity):
+            mat_sim = 0.5 * (1 + self.distace_sim(embeddings))
+        else:
+            # mat_sim = 0.5 * (1 + self.distace_sim(project(x_emb_samples)))
+            mat_sim = torch.exp(-self.distace_sim(embeddings))
+
+        mat_lca = self.distance_lca(self._rescale_emb(embeddings))
+        wij = mat_sim[anchor_idx, positive_idx]
+        wik = mat_sim[anchor_idx, negative_idx]
+        wjk = mat_sim[positive_idx, negative_idx]
+
+        dij = mat_lca[anchor_idx, positive_idx]
+        dik = mat_lca[anchor_idx, negative_idx]
+        djk = mat_lca[positive_idx, negative_idx]
+
+        # loss proposed by Chami et al.
+        sim_triplet = torch.stack([wij, wik, wjk]).T
+        lca_triplet = torch.stack([dij, dik, djk]).T
+        weights = torch.softmax(lca_triplet / self.temperature, dim=-1)
+
+        w_ord = torch.sum(sim_triplet * weights, dim=-1, keepdim=True)
+        total = torch.sum(sim_triplet, dim=-1, keepdim=True) - w_ord
+
+        loss_triplet_lca = torch.mean(total) + mat_sim.mean()
+
+        loss_triplet_sim = self.loss_triplet_sim(embeddings, labels)
+
+        return {
+            "loss_lca": {
+                "losses": loss_triplet_lca,
+                "indices": (anchor_idx, positive_idx, negative_idx),
+                "reduction_type": "already_reduced",
+            },
+            "loss_sim": {
+                "losses": loss_triplet_sim,
+                "indices": None,
+                "reduction_type": "already_reduced",
+            },
+        }
 
 
 def make_pairs(labels: torch.Tensor, idx: torch.Tensor):
