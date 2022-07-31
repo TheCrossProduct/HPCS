328524f72ad77cf124b48198e2c204cbecf34efd
Leonardo Gigli <leonardo.gigli@thecrossproduct.com>
2022-07-31 23:43:48 +0200

    [TRAIN][3D] Wip code refactoring to update code to pytorch_lightning 1.6.5

--- train_3Dhyperbolic.py
+++ train_3Dhyperbolic.py
@@ -8,7 +8,7 @@ from torch_geometric.loader import DataLoader
 
 import pytorch_lightning as pl
 from pytorch_lightning.utilities import rank_zero_only
-from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning.loggers import WandbLogger
 from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
 
 from hpcs.nn.models._hyp_hc import SimilarityHypHC
@@ -23,17 +23,8 @@ from hpcs.nn.models.networks._mlp import MLP
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--logdir', default='hyperbolic', type=str, help='dirname for logs')
+    parser.add_argument('--logdir', default='logs', type=str, help='dirname for logs')
     parser.add_argument('--data', default='shapenet', type=str, help='name of dataset to use')
-    # parser.add_argument('--train_samples', default=100, type=int, help='number of samples in training set')
-    # parser.add_argument('--valid_samples', default=10, type=int, help='number of samples in valid set')
-    # parser.add_argument('--test_samples', default=10, type=int, help='number of samples in test set')
-    # parser.add_argument('--max_points', default=300, type=int, help='number of points in each sample')
-    # parser.add_argument('--num_labels', default=0.3, type=float, help='number/ratio of labels to use in each sample')
-    # parser.add_argument('--min_noise', default=0.12, type=float, help='min value of noise to use')
-    # parser.add_argument('--max_noise', default=0.15, type=float, help='max value of noise to use')
-    # parser.add_argument('--cluster_std', default=0.1, type=float, help='std blobs')
-    # parser.add_argument('--num_blobs', default=3, type=int, help='number of blobs in blob/aniso/varied')
     parser.add_argument('--model', default='dgcnn2', type=str, help='model to use to extract features')
     parser.add_argument('--embedder', help='if True add a an embedding model from the feature space to B2', action='store_true')
     parser.add_argument('--k', default=10, type=int, help='if model dgcnn, k is the number of neigh to take into account')
@@ -62,15 +53,6 @@ if __name__ == "__main__":
     logdir = args.logdir
     dataname = args.data
     epochs = args.epochs
-    # train_samples = args.train_samples
-    # valid_samples = args.valid_samples
-    # test_samples = args.test_samples
-    # num_labels = args.num_labels
-    # max_points = args.max_points
-    # min_noise = args.min_noise
-    # max_noise = args.max_noise
-    # cluster_std = args.cluster_std
-    # num_blobs = args.num_blobs
     model_name = args.model
     embedder = args.embedder
     k = args.k
@@ -93,7 +75,7 @@ if __name__ == "__main__":
 
 
     category = 'Airplane'  # Pass in `None` to train on all categories.
-    path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', 'ShapeNet')
+    path = osp.join(osp.dirname(osp.realpath(__file__)), 'data', 'ShapeNet')
 
     pre_transform, transform = T.NormalizeScale(), T.FixedPoints(fixed_points)
     train_dataset = ShapeNet(path, category, split='train', transform=transform, pre_transform=pre_transform)
@@ -103,26 +85,6 @@ if __name__ == "__main__":
     train_loader = DataLoader(train_dataset, batch_size=batch, shuffle=True, num_workers=num_workers)
     valid_loader = DataLoader(valid_dataset, batch_size=batch, shuffle=False, num_workers=num_workers)
     test_loader = DataLoader(test_dataset, batch_size=batch, shuffle=False, num_workers=num_workers)
-
-
-    class MyTensorBoardLogger(TensorBoardLogger):
-        def __init__(self, *args, **kwargs):
-            super(MyTensorBoardLogger, self).__init__(*args, **kwargs)
-
-        def log_hyperparams(self, *args, **kwargs):
-            pass
-
-        @rank_zero_only
-        def log_hyperparams_metrics(self, params: dict, metrics: dict) -> None:
-            from torch.utils.tensorboard.summary import hparams
-            params = self._convert_params(params)
-            exp, ssi, sei = hparams(params, metrics)
-            writer = self.experiment._get_file_writer()
-            writer.add_summary(exp)
-            writer.add_summary(ssi)
-            writer.add_summary(sei)
-            # some alternative should be added
-            self.hparams.update(params)
 
     if len(args.gpu):
         gpu = [int(g) for g in args.gpu.split(',')]
@@ -170,13 +132,9 @@ if __name__ == "__main__":
                             anneal_step=anneal_step,
                             plot_every=plot_every)
 
-    logger = MyTensorBoardLogger(logdir, name=dataname)
+    # logger = MyTensorBoardLogger(logdir, name=dataname)
+    logger = WandbLogger(name=dataname, save_dir=os.path.join(logdir, "HPCS"), project="HPCS")
     model_params = {'dataset': dataname,
-                    # 'ratio_labels': num_labels,
-                    # 'min_noise': min_noise if dataname in ['moons', 'circles'] else None,
-                    # 'max_noise': max_noise if dataname in ['moons', 'circles'] else None,
-                    # 'cluster_std': cluster_std if dataname in ['blobs', 'varied', 'aniso'] else None,
-                    # 'num_blobs': num_blobs if dataname in ['blobs', 'varied', 'aniso'] else '-1',
                     'model': model_name,
                     'embedder': 'True' if embedder else 'False',
                     'k': k if model_name == 'dgcnn' else -1,
@@ -202,7 +160,7 @@ if __name__ == "__main__":
                'ari': 0.0, 'ari-std': 0.0,
                'best_k': 0.0, 'std_k': 0.0}
 
-    logger.log_hyperparams_metrics(params=model_params, metrics=metrics)
+    # logger.log_metrics(metrics)
     savedir = os.path.join(logger.save_dir, logger.name, 'version_' + str(logger.version), 'checkpoints')
     # call backs for trainer
     checkpoint_callback = ModelCheckpoint(dirpath=savedir, verbose=True)
@@ -214,17 +172,15 @@ if __name__ == "__main__":
         mode='min')
 
 
-    trainer = pl.Trainer(gpus=gpu, max_epochs=epochs,
-                         checkpoint_callback=checkpoint_callback,
-                         callbacks=early_stop_callback,
+    trainer = pl.Trainer(gpus=gpu, max_epochs=epochs, limit_train_batches=20, log_every_n_steps=10,
+                         callbacks=[early_stop_callback, checkpoint_callback],
                          logger=logger,
-                         track_grad_norm=2,
-                         distributed_backend=distributed_backend,
-                         replace_sampler_ddp=replace_sampler_ddp)
+                         # track_grad_norm=2
+                         )
 
 
     trainer.fit(model, train_loader, valid_loader)
 
     print("End Training")
 
-    results = trainer.test(model, test_loader)
+    # results = trainer.test(model, test_loader)
