48b11b9799a02a18be05aaca64936e9b78bfd42e
Leonardo Gigli <leonardo.gigli@thecrossproduct.com>
2023-01-20 18:12:53 +0100

    [UTILS] Added math functions to generate a 3D random rotation

--- hpcs/utils/math.py
+++ hpcs/utils/math.py
@@ -1,6 +1,46 @@
 """Math utils functions."""
-
+from typing import Union
 import torch
+
+# ################# rot 3d ########################
+def yaw_rot(alpha: Union[float, torch.Tensor]) -> torch.Tensor:
+    if isinstance(alpha, float):
+        alpha = torch.Tensor([alpha])
+
+    return torch.Tensor([
+        [torch.cos(alpha), -torch.sin(alpha), 0],
+        [torch.sin(alpha),  torch.cos(alpha), 0],
+        [0, 0, 1],
+    ])
+
+
+def pitch_rot(beta: Union[float, torch.Tensor]) -> torch.Tensor:
+    if isinstance(beta, float):
+        beta = torch.Tensor([beta])
+
+    return torch.Tensor([
+        [torch.cos(beta), 0, torch.sin(beta)],
+        [0, 1, 0],
+        [-torch.sin(beta), 0, torch.cos(beta)],
+    ])
+
+
+def roll_rot(gamma: Union[float, torch.Tensor]) -> torch.Tensor:
+    if isinstance(gamma, float):
+        gamma = torch.Tensor([gamma])
+
+    return torch.Tensor([
+        [1, 0, 0],
+        [0, torch.cos(gamma), -torch.sin(gamma)],
+        [0, torch.sin(gamma),  torch.cos(gamma)],
+    ])
+
+
+def rot_3D(alpha: float, beta: float, gamma: float) -> torch.Tensor:
+    R_yaw = yaw_rot(alpha)
+    R_pitch = pitch_rot(beta)
+    R_roll = roll_rot(gamma)
+    return R_yaw @ R_pitch @ R_roll
 
 
 # ################# tanh ########################
