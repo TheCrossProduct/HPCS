194374980d2d431c8f9743ec76e1bfe7b53b8e5c
Leonardo Gigli <leonardo.gigli@thecrossproduct.com>
2023-01-21 11:07:33 +0100

    [TEST] Updated invariance test

--- test/test_rotation_invariance.py
+++ test/test_rotation_invariance.py
@@ -1,51 +1,50 @@
 import unittest
 import torch
-import numpy as np
 import os.path as osp
+from torch.utils.data import DataLoader
+from torch.nn import functional as F
 from hpcs.nn.dgcnn import VN_DGCNN_partseg
+from hpcs.utils.math import rot_3D
+from hpcs.hyp_hc import remap_labels
 from data.ShapeNet.ShapeNetDataLoader import PartNormalDataset
 
-def get_rot(theta):
-    return np.array([
-        [np.cos(theta), np.sin(theta), 0],
-        [-np.sin(theta), np.cos(theta), 0],
-        [0, 0, 1],
-    ])
-
-def rotate_sample(x, R):
-    z = x.T
-    return (R@z).T
-
-def to_categorical(y, num_classes):
-    """ 1-hot encodes a tensor """
-    new_y = torch.eye(num_classes)[y.cpu().data.numpy(),]
-    if (y.is_cuda):
-        return new_y.cuda()
-    return new_y
 
 class TestRotationInvariance(unittest.TestCase):
     def setUp(self) -> None:
-        path = osp.join(osp.dirname(osp.realpath(__file__)), '../../data', 'ShapeNet/raw')
+        path = osp.join(osp.dirname(osp.realpath(__file__)), '../data', 'ShapeNet/raw')
         self.num_classes = 16
-        self.nn = VN_DGCNN_partseg(in_channels=3, out_features=2, k=10, dropout=0.5, pooling='mean')
+        self.nn = VN_DGCNN_partseg(in_channels=3, out_features=2, k=10, dropout=0.5, pooling='mean', num_class=self.num_classes)
 
         train_dataset = PartNormalDataset(root=path, npoints=256, split='train', class_choice='Airplane')
-        airplane_sample = train_dataset.__getitem__(0)
+        train_loader = DataLoader(train_dataset, batch_size=2, shuffle=True, num_workers=1, drop_last=True)
 
-        self.points, self.label, self.target = airplane_sample  ## 3D coordinates of the airplane
+        # getting one sample from training set
+        for airplane_sample in train_loader:
+            points, label, target = airplane_sample  ## 3D coordinates of the airplane
+            break
+        self.points = points.float()
+        self.label = label.long()
+        self.target = target.long()
+
+        num_parts = self.num_classes
+        batch_class_vector = []
+        for object in self.target:
+            parts = F.one_hot(remap_labels(torch.unique(object)), num_parts)
+            class_vector = parts.sum(dim=0).float()
+            batch_class_vector.append(class_vector)
+
+        self.decode_vector = torch.stack(batch_class_vector)
 
     def test_invariance(self):
-        R = get_rot(np.random.rand())
-        R = torch.from_numpy(R)
-        label = torch.from_numpy(self.label)
-        label = to_categorical(label, self.num_classes)
-        label = label[None, ...]
-        points = torch.from_numpy(self.points)
-        points = points.transpose(2, 1)
-        points = points[None, ...] ## moves from [n_pts, 3] -> [1, n_, 3pts, 3]
+        angles = 2 * torch.pi * torch.rand([3])
+        R = rot_3D(angles[0], angles[1], angles[2])
+        print(f"Point size: {self.points.size}")
+        self.assertEqual(self.points.size(), (2, 256, 3))
+        rot_points = torch.empty_like(self.points)
+        for i in range(self.points.size(0)):
+            rot_points[i] = (R@self.points[i].T).T
 
-        y_1 = self.nn(points, label)
-        y_2 = self.nn((R @ points.T).T, label)  # need to verify the dimensions
-        print(y_1)
+        y_1 = self.nn(self.points.transpose(2, 1), self.decode_vector)
+        y_2 = self.nn(rot_points.transpose(2, 1), self.decode_vector)
 
-        self.assertAlmostEqual(torch.linalg.norm(y_1-y_2), 0.0)
+        self.assertAlmostEqual(torch.linalg.norm(y_1-y_2).detach().numpy(), 0.0)
