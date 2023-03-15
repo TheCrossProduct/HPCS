f7f2d98861bb9d576763c4830ca0e3786a93e9c8
Leonardo Gigli <leonardo.gigli@thecrossproduct.com>
2023-03-15 18:33:06 +0100

    updated script to infer on pretrained models

--- infer.py
+++ infer.py
@@ -1,3 +1,4 @@
+import warnings
 from typing import Optional, Union
 import argparse
 import os
@@ -48,7 +49,9 @@ def infer_partnet(model_path : str, category: str, level: int, num_points: int =
                   test_batches: Optional[Union[int, float]] = None):
   num_workers = mp.cpu_count()
   data_folder = get_partnet_path()
-  path = os.path.realpath(os.path.join('..', data_folder, '%s-%d' % (category, level), 'test_files.txt'))
+  if category.lower() not in model_path.lower():
+    print(f"Be careful the chosen model {model_path} could not be adapted for this category: {category.title()}")
+  path = os.path.realpath(os.path.join('..', data_folder, '%s-%d' % (category.title(), level), 'test_files.txt'))
 
   test_dataset = PartNetDataset(path, num_points)
   test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
@@ -61,23 +64,30 @@ def infer_partnet(model_path : str, category: str, level: int, num_points: int =
 def main():
   parser = argparse.ArgumentParser(description='Foo Bar')
   subparsers = parser.add_subparsers(dest='command', help='Dataset on which run inference', required=True)
-  parser.add_argument('--num_points', type=int,)
 
   parser_shapenet = subparsers.add_parser('shapenet', help='Infer model on Shapenet dataset')
-  parser_shapenet.add_argument('category', help='X')
   parser_shapenet.set_defaults(func=infer_shapenet)
 
   parser_partnet = subparsers.add_parser('partnet', help='Infer model on Partnet dataset')
-  parser_partnet.add_argument('x', help='X')
-  parser_partnet.add_argument('y', help='Y')
+  parser_partnet.add_argument('--category', type=str, help='Category of object on which infer')
+  parser_partnet.add_argument('--level', type=int, help='Level of segmentation on which test')
   parser_partnet.set_defaults(func=infer_partnet)
 
+  for subparser in [parser_shapenet, parser_partnet]:
+    subparser.add_argument('--num_points', default=1024, type=int, help='Number of points in each sample')
+    subparser.add_argument('--batch_size', default=1, type=int, help='Batch Size')
+    subparser.add_argument('--test_batches', default=None, type=float, help='Batch Size')
+    subparser.add_argument('--model_path', help='Path to pretrained model to test')
+    subparser.add_argument('--plot', action='store_true', help='Add this flag to plot results')
+
   args = parser.parse_args()
-  print(args)
-  # args_ = vars(args).copy()
-  # args_.pop('command', None)
-  # args_.pop('func', None)
-  # args.func(**args_)
+  args_ = vars(args).copy()
+  args_.pop('command', None)
+  args_.pop('func', None)
+  args_['test_batches'] = int(args_['test_batches']) if \
+    (args_['test_batches'] is not None and args_['test_batches'].is_integer()) else args_['test_batches']
+  print(f"Calling {args.func.__name__}, with the folling arguments {args_}")
+  args.func(**args_)
 
 if __name__ == '__main__':
   main()
